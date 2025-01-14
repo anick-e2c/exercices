5dc585dc27bf0818d3a69fe07aeec2a31712143f
koueAnicet <anicetkoue1@gmail.com>
2025-01-14 19:18:37 +0000

    API reusit

--- api-test/api.js
+++ api-test/api.js
@@ -1,9 +1,13 @@
  // requete de l'api avec la methode asynchrone
-
-/* async function fetchData() {
+const cityElement = document.getElementById('city')
+const tempElemnt = document.getElementById('temperature')
+const envElemnt = document.getElementById('environnement')
+const apiKey = "4a6c0b808c51edc16150faf5ae72c419";
+/* 
+async function fetchData() {
     try {
         //Requete GET de l'API 
-        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=', {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4a6c0b808c51edc16150faf5ae72c419', {
             method: 'GET',
             headers: {
                 'Authorization': '4a6c0b808c51edc16150faf5ae72c419',
@@ -21,52 +25,65 @@
     } catch (error) {
         console.error('Erreur', error);
     }
-} */
+} 
 
 // appel de la fonction async
-/* let infos = document.getElementById('infos');
-console.log(fetchData()) */
+//let infos = document.getElementById('infos');
+let resp = fetchData();
+console.log(resp);
 //fetchData(); 
-
+ */
 /* fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4a6c0b808c51edc16150faf5ae72c419')
-.then(Response => Response.json())
+.then(response => response.json())
 .then(data => {
     console.log(data);
 })
 .catch(error => {
     console.error('Erreur', error);
-}) */
+}) 
 
 // Autre methode
-
-/* async function fetchWeather() {
-    const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4a6c0b808c51edc16150faf5ae72c419';
+*/
+  async function fetchWeather() {
 
     try {
+        // Prend en compte A ville 
+        const enterCity = prompt('Entrez le nom d\'une ville:', 'une ville par ici...');
+
+        if (!enterCity) {
+            console.log('Entrez le nom d\'une ville:');
+            return;
+        }
+
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${enterCity}&appid=${apiKey}&units=metric&lang=fr`;
+
         //Faire la requete GET à l'API
         const response = await fetch(apiUrl);
 
-        console.log(response.json());
+       
+
         //Verifier si la response est OK (statut 200)
         if(!response.ok) {
             throw new Error(`Erreur HTTP: ${response.status}`);
 
         }
-        // C onvertir la reponse en JSON
+        // Convertir la reponse en JSON
         const data = await response.json();
 
         // Affiche les données dans la console 
         console.log('Données météo:', data);
 
+        // Mis à jour des element
+        cityElement.textContent = `${data.name}, ${data.sys.country}`;
+        tempElemnt.textContent = `${data.main.temp}°C`;
+        envElemnt.textContent = `${data.weather[0].description}`;
+
     }catch (error) {
         // Gérer les erreurs
-        console.error('Erreur lors de la recuperation des données:', error);
+        console.error('Erreur lors de la recuperation des données:', error.message);
     }
 }
 
 // Apperler la fonction pour exécuter la requète
-console.log(fetchWeather()) */
-
-const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4a6c0b808c51edc16150faf5ae72c419';
-const response = await fetch(apiUrl);
-console.log(response);
+const apires = fetchWeather();
+console.log(apires);
