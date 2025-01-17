6629606b3edcdb41ea7cb744df661a2ca581e23d
koueAnicet <anicetkoue1@gmail.com>
2025-01-17 19:44:23 +0000

    fonctionnalité complète

--- calculator/script-calculator.js
+++ calculator/script-calculator.js
@@ -1,9 +1,23 @@
+//va initialiser pour suivre si une nouvelle saisie commence
+let isNewInput = false;
+
 function addToScreen(value) {
-    document.getElementById('screen').value += value;
+    //document.getElementById('screen').value += value;
+
+    const display = document.getElementById('screen');
+
+    // Si c'est une nouvelle saisie, réinitialiser l'écran
+    if (isNewInput && !isNaN(value)) {
+        display.value = '';
+        isNewInput = false; 
+    }
+    display.value += value;
 }
 
 function clearScreen() {
     document.getElementById('screen').value = '';
+
+    isNewInput = false; // réinitialiser la variable
     
 }
 
@@ -19,7 +33,12 @@ function pourcentage(values) {
 function calculate() {
     try {
         document.getElementById('screen').value = eval(document.getElementById('screen').value);
+
+        isNewInput = true;
+        // Pour marquer le prochain chiffre doit commencer une nouvelle saisie
     } catch {
-        document.getElementById('screen').value = 'Erreur'
+        document.getElementById('screen').value = 'Erreur';
+        isNewInput = true;
+        // Pour marquer le prochain chiffre doit commencer une nouvelle saisie
     }
 }
